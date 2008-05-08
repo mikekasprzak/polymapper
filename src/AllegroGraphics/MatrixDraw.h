a9d904fa8cc625f3705bbad1d50cc36edf6beeac
mike <mike@b38dc06d-f929-334a-8341-2d1f71cecdda>
2008-05-08 22:53:47 +0000

    Added a number of functions for only drawing normals of various shapes.
    git-svn-id: svn://10.0.50.55/polymapper/trunk@22 b38dc06d-f929-334a-8341-2d1f71cecdda

--- src/AllegroGraphics/MatrixDraw.h
+++ src/AllegroGraphics/MatrixDraw.h
@@ -205,6 +205,86 @@ inline void MatrixRectWithNormals( BITMAP* Target, const Matrix3x3& Matrix, cons
 
 }
 // - ------------------------------------------------------------------------------------------ - //
+inline void MatrixRectNormals( BITMAP* Target, const Matrix3x3& Matrix, const Rect2D& Rect, const Real NormalLength = 8, const int NormalColor = makecol(255,255,255)  ) {
+	// Get our points //
+	Vector2D Point[4];
+	Point[0] = Rect.P1();
+	Point[1] = Vector2D( Rect.P2().x, Rect.P1().y );
+	Point[2] = Rect.P2();
+	Point[3] = Vector2D( Rect.P1().x, Rect.P2().y );
+	
+	// Transform them by the matrix //
+	// TODO: Can be optimized to one transformation (because it's 2D, and Tangent) //
+	Point[0] = Point[0].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	Point[1] = Point[1].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	Point[2] = Point[2].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	Point[3] = Point[3].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	
+	// TODO: Can be optimized to use same general transformation information as the center //
+	//   plus "one transformation" method theorized above. //
+	
+	{
+		// Take Point A to B //
+		Vector2D AB = Point[1] - Point[0];
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = Point[0] + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+	
+	{
+		// Take Point A to B //
+		Vector2D AB = Point[2] - Point[1];
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = Point[1] + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+	
+	{
+		// Take Point A to B //
+		Vector2D AB = Point[3] - Point[2];
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = Point[2] + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+	
+	{
+		// Take Point A to B //
+		Vector2D AB = Point[0] - Point[3];
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = Point[3] + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+
+}
+// - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
 inline void MatrixRectEdge( BITMAP* Target, const Matrix3x3& Matrix, const int Index, const Rect2D& Rect, const int Color ) {
@@ -231,6 +311,85 @@ inline void MatrixRectEdge( BITMAP* Target, const Matrix3x3& Matrix, const int I
 		line( Target, (int)(Point[2].x), (int)(Point[2].y), (int)(Point[3].x), (int)(Point[3].y), Color );
 	else if ( Index == 3 )
 		line( Target, (int)(Point[3].x), (int)(Point[3].y), (int)(Point[0].x), (int)(Point[0].y), Color );
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline void MatrixRectEdgeNormal( BITMAP* Target, const Matrix3x3& Matrix, const int Index, const Rect2D& Rect, const Real NormalLength = 8, const int NormalColor = makecol(255,255,255) ) {
+	// Get our points //
+	Vector2D Point[4];
+	Point[0] = Rect.P1();
+	Point[1] = Vector2D( Rect.P2().x, Rect.P1().y );
+	Point[2] = Rect.P2();
+	Point[3] = Vector2D( Rect.P1().x, Rect.P2().y );
+	
+	// Transform them by the matrix //
+	// TODO: Can be optimized to one transformation (because it's 2D, and Tangent) //
+	Point[0] = Point[0].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	Point[1] = Point[1].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	Point[2] = Point[2].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	Point[3] = Point[3].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	
+	// TODO: Can be optimized to use same general transformation information as the center //
+	//   plus "one transformation" method theorized above. //
+	if ( Index == 0 )
+	{
+		// Take Point A to B //
+		Vector2D AB = Point[1] - Point[0];
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = Point[0] + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+	else if ( Index == 1 )	
+	{
+		// Take Point A to B //
+		Vector2D AB = Point[2] - Point[1];
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = Point[1] + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+	else if ( Index == 2 )	
+	{
+		// Take Point A to B //
+		Vector2D AB = Point[3] - Point[2];
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = Point[2] + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+	else if ( Index == 3 )	
+	{
+		// Take Point A to B //
+		Vector2D AB = Point[0] - Point[3];
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = Point[3] + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
 }
 // - ------------------------------------------------------------------------------------------ - //
 
@@ -486,6 +645,162 @@ inline void MatrixOpenPolygonWithInvNormals( BITMAP* Target, const Matrix3x3& Ma
 }
 // - ------------------------------------------------------------------------------------------ - //
 
+// - ------------------------------------------------------------------------------------------ - //
+inline void MatrixClosedPolygonNormals( BITMAP* Target, const Matrix3x3& Matrix, const Vector2D* Poly, const size_t PolyCount, const Real NormalLength = 8, const int NormalColor = makecol(255,255,255) ) {
+	// Bail if less than 2 points //
+	if ( PolyCount < 2 )
+		return;
+	
+	// Transform the first point //
+	Vector2D StartPoint = Poly[0].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	
+	Vector2D LastPoint;
+	Vector2D NextPoint = StartPoint;
+	
+	// For every point in the Polygon //
+	for ( size_t idx = 0; idx < PolyCount-1; idx++ ) {
+		// Last becomes Next, and transform a new Next //
+		LastPoint = NextPoint;
+		NextPoint = Poly[idx+1].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+		
+		// Take Point A to B //
+		Vector2D AB = NextPoint - LastPoint;
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = LastPoint + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+		
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+
+	// Take Point A to B //
+	Vector2D AB = StartPoint - NextPoint;
+	Real Length = AB.NormalizeRet();
+	
+	// Calculate Center //
+	Vector2D Center = NextPoint + (AB * (Length * Real::Half));
+
+	// Calculate Normal's position //
+	Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+	
+	// Draw the Normal //
+	line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline void MatrixClosedPolygonInvNormals( BITMAP* Target, const Matrix3x3& Matrix, const Vector2D* Poly, const size_t PolyCount, const Real NormalLength = 8, const int NormalColor = makecol(255,255,255) ) {
+	// Bail if less than 2 points //
+	if ( PolyCount < 2 )
+		return;
+	
+	// Transform the first point //
+	Vector2D StartPoint = Poly[0].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	
+	Vector2D LastPoint;
+	Vector2D NextPoint = StartPoint;
+	
+	// For every point in the Polygon //
+	for ( size_t idx = 0; idx < PolyCount-1; idx++ ) {
+		// Last becomes Next, and transform a new Next //
+		LastPoint = NextPoint;
+		NextPoint = Poly[idx+1].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+		
+		// Take Point A to B //
+		Vector2D AB = NextPoint - LastPoint;
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = LastPoint + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (-AB.Tangent() * NormalLength );
+		
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+
+	// Take Point A to B //
+	Vector2D AB = StartPoint - NextPoint;
+	Real Length = AB.NormalizeRet();
+	
+	// Calculate Center //
+	Vector2D Center = NextPoint + (AB * (Length * Real::Half));
+
+	// Calculate Normal's position //
+	Vector2D Normal = Center + (-AB.Tangent() * NormalLength );
+	
+	// Draw the Normal //
+	line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline void MatrixOpenPolygonNormals( BITMAP* Target, const Matrix3x3& Matrix, const Vector2D* Poly, const size_t PolyCount, const Real NormalLength = 8, const int NormalColor = makecol(255,255,255) ) {
+	// Bail if less than 2 points //
+	if ( PolyCount < 2 )
+		return;
+	
+	// Transform the first point //
+	Vector2D StartPoint = Poly[0].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	
+	Vector2D LastPoint;
+	Vector2D NextPoint = StartPoint;
+	
+	// For every point in the Polygon //
+	for ( size_t idx = 0; idx < PolyCount-1; idx++ ) {
+		// Last becomes Next, and transform a new Next //
+		LastPoint = NextPoint;
+		NextPoint = Poly[idx+1].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+		
+		// Take Point A to B //
+		Vector2D AB = NextPoint - LastPoint;
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = LastPoint + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+		
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline void MatrixOpenPolygonInvNormals( BITMAP* Target, const Matrix3x3& Matrix, const Vector2D* Poly, const size_t PolyCount, const Real NormalLength = 8, const int NormalColor = makecol(255,255,255) ) {
+	// Bail if less than 2 points //
+	if ( PolyCount < 2 )
+		return;
+	
+	// Transform the first point //
+	Vector2D StartPoint = Poly[0].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	
+	Vector2D LastPoint;
+	Vector2D NextPoint = StartPoint;
+	
+	// For every point in the Polygon //
+	for ( size_t idx = 0; idx < PolyCount-1; idx++ ) {
+		// Last becomes Next, and transform a new Next //
+		LastPoint = NextPoint;
+		NextPoint = Poly[idx+1].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+		
+		// Take Point A to B //
+		Vector2D AB = NextPoint - LastPoint;
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = LastPoint + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (-AB.Tangent() * NormalLength );
+		
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+}
+// - ------------------------------------------------------------------------------------------ - //
+
 
 // - ------------------------------------------------------------------------------------------ - //
 inline void MatrixEdgedPolygon( BITMAP* Target, const Matrix3x3& Matrix, const Vector2D* Poly, const bool* PolygonEnabled, const size_t PolyCount, const int Color ) {
@@ -630,6 +945,111 @@ inline void MatrixEdgedPolygonWithInvNormals( BITMAP* Target, const Matrix3x3& M
 	}
 }
 // - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+inline void MatrixEdgedPolygonNormals( BITMAP* Target, const Matrix3x3& Matrix, const Vector2D* Poly, const bool* PolygonEnabled, const size_t PolyCount, const Real NormalLength = 8, const int NormalColor = makecol(255,255,255) ) {
+	// Bail if less than 2 points //
+	if ( PolyCount < 2 )
+		return;
+	
+	// Transform the first point //
+	Vector2D StartPoint = Poly[0].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	
+	Vector2D LastPoint;
+	Vector2D NextPoint = StartPoint;
+	
+	// For every point in the Polygon //
+	for ( size_t idx = 0; idx < PolyCount-1; idx++ ) {
+		// Last becomes Next, and transform a new Next //
+		LastPoint = NextPoint;
+		NextPoint = Poly[idx+1].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+		
+		// Test if this particular edge is enabled //
+		if ( PolygonEnabled[ idx ] ) {
+			// Take Point A to B //
+			Vector2D AB = NextPoint - LastPoint;
+			Real Length = AB.NormalizeRet();
+			
+			// Calculate Center //
+			Vector2D Center = LastPoint + (AB * (Length * Real::Half));
+		
+			// Calculate Normal's position //
+			Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+			
+			// Draw the Normal //
+			line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+		}
+	}
+
+	// Draw only if this edge is enabled //
+	if ( PolygonEnabled[ PolyCount-1 ] ) {
+		// Take Point A to B //
+		Vector2D AB = StartPoint - NextPoint;
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = NextPoint + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (AB.Tangent() * NormalLength );
+		
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+}
+// - ------------------------------------------------------------------------------------------ - //
+inline void MatrixEdgedPolygonInvNormals( BITMAP* Target, const Matrix3x3& Matrix, const Vector2D* Poly, const bool* PolygonEnabled, const size_t PolyCount, const Real NormalLength = 8, const int NormalColor = makecol(255,255,255) ) {
+	// Bail if less than 2 points //
+	if ( PolyCount < 2 )
+		return;
+	
+	// Transform the first point //
+	Vector2D StartPoint = Poly[0].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+	
+	Vector2D LastPoint;
+	Vector2D NextPoint = StartPoint;
+	
+	// For every point in the Polygon //
+	for ( size_t idx = 0; idx < PolyCount-1; idx++ ) {
+		// Last becomes Next, and transform a new Next //
+		LastPoint = NextPoint;
+		NextPoint = Poly[idx+1].ToHomoVector3D().ApplyMatrix( Matrix ).ToVector2D();
+		
+		// Test if this particular edge is enabled //
+		if ( PolygonEnabled[ idx ] ) {
+			// Take Point A to B //
+			Vector2D AB = NextPoint - LastPoint;
+			Real Length = AB.NormalizeRet();
+			
+			// Calculate Center //
+			Vector2D Center = LastPoint + (AB * (Length * Real::Half));
+		
+			// Calculate Normal's position //
+			Vector2D Normal = Center + (-AB.Tangent() * NormalLength );
+			
+			// Draw the Normal //
+			line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+		}
+	}
+
+	// Draw only if this edge is enabled //
+	if ( PolygonEnabled[ PolyCount-1 ] ) {
+		// Take Point A to B //
+		Vector2D AB = StartPoint - NextPoint;
+		Real Length = AB.NormalizeRet();
+		
+		// Calculate Center //
+		Vector2D Center = NextPoint + (AB * (Length * Real::Half));
+	
+		// Calculate Normal's position //
+		Vector2D Normal = Center + (-AB.Tangent() * NormalLength );
+		
+		// Draw the Normal //
+		line( Target, (int)Center.x, (int)Center.y, (int)Normal.x, (int)Normal.y, NormalColor );
+	}
+}
+// - ------------------------------------------------------------------------------------------ - //
+
 
 
 // - ------------------------------------------------------------------------------------------ - //

266d46fc3fcc60a33f12982e7c375692e26a053a
mike <mike@b38dc06d-f929-334a-8341-2d1f71cecdda>
2008-06-13 01:27:30 +0000

    Commented out the old work in progress breakdown of the PointVsSphere Info structure thing.
    git-svn-id: svn://10.0.50.55/polymapper/trunk@66 b38dc06d-f929-334a-8341-2d1f71cecdda

--- src/AdvancedGeometry/PointVsSphere2D.h
+++ src/AdvancedGeometry/PointVsSphere2D.h
@@ -15,62 +15,62 @@ inline bool Test_Point_Vs_Sphere2D( const Vector2D& Pos, const Vector2D& VsPos, 
 }
 // - ------------------------------------------------------------------------------------------ - //
 
-// - ------------------------------------------------------------------------------------------ - //
-// TODO: The solving stuff below isn't done.  I have to figure out exactly what solving actually //
-//   needs.  I thought it wanted to know if it was inside the sphere, but I think it actually //
-//   wants something to do with the distance between the spheres.  I.e. a vector to say "how" to //
-//   solve, by pushing outward. //
-// - ------------------------------------------------------------------------------------------ - //
-struct cPointVsSphere2D {
-	// --- Start Part ------------------ //
-	Vector2D Diff;
-	Real MagnitudeSquared;
-	Real Radius;
-	
-	inline const Real RadiusSquared() const {
-		return Radius * Radius;
-	}
-
-	// --- Solve Part ------------------ //
-	Real Magnitude;
-		
-	//bool InsideSphere;
-};
-// - ------------------------------------------------------------------------------------------ - //
-
-// - ------------------------------------------------------------------------------------------ - //
-inline cPointVsSphere2D StartPointVsSphere2D( const Vector2D& Pos, const Vector2D& VsPos, const Real VsRadius ) {
-	cPointVsSphere2D Info;
-	
-	Info.Diff = VsPos - Pos;
-	Info.MagnitudeSquared = Info.Diff.MagnitudeSquared();
-	Info.Radius = VsRadius;
-	
-	return Info;
-}
-// - ------------------------------------------------------------------------------------------ - //
-inline bool TestPointVsSphere2D( const cPointVsSphere2D& Info ) {
-	return Info.MagnitudeSquared < Info.RadiusSquared();
-}
-// - ------------------------------------------------------------------------------------------ - //
-inline cPointVsSphere2D SolvePointVsSphere2D( const cPointVsSphere2D& _Info ) {
-	cPointVsSphere2D Info = _Info;
-	
-	Info.Magnitude = Info.MagnitudeSquared.Sqrt();
-	
-	// TODO: More stuff needs to be done here, to correctly be considered a solve //
-	
-	//Info.InsideSphere = Info.MagnitudeSquared < Info.RadiusSquared;
-	
-	
-	return Info;
-}
-// - ------------------------------------------------------------------------------------------ - //
-inline cPointVsSphere2D SolvePointVsSphere2D( const Vector2D& Pos, const Vector2D& VsPos, const Real VsRadius ) {
-	cPointVsSphere2D Info = StartPointVsSphere2D( Pos, VsPos, VsRadius );
-	return SolvePointVsSphere2D( Info );
-}
-// - ------------------------------------------------------------------------------------------ - //
+//// - ------------------------------------------------------------------------------------------ - //
+//// TODO: The solving stuff below isn't done.  I have to figure out exactly what solving actually //
+////   needs.  I thought it wanted to know if it was inside the sphere, but I think it actually //
+////   wants something to do with the distance between the spheres.  I.e. a vector to say "how" to //
+////   solve, by pushing outward. //
+//// - ------------------------------------------------------------------------------------------ - //
+//struct cPointVsSphere2D {
+//	// --- Start Part ------------------ //
+//	Vector2D Diff;
+//	Real MagnitudeSquared;
+//	Real Radius;
+//	
+//	inline const Real RadiusSquared() const {
+//		return Radius * Radius;
+//	}
+//
+//	// --- Solve Part ------------------ //
+//	Real Magnitude;
+//		
+//	//bool InsideSphere;
+//};
+//// - ------------------------------------------------------------------------------------------ - //
+//
+//// - ------------------------------------------------------------------------------------------ - //
+//inline cPointVsSphere2D StartPointVsSphere2D( const Vector2D& Pos, const Vector2D& VsPos, const Real VsRadius ) {
+//	cPointVsSphere2D Info;
+//	
+//	Info.Diff = VsPos - Pos;
+//	Info.MagnitudeSquared = Info.Diff.MagnitudeSquared();
+//	Info.Radius = VsRadius;
+//	
+//	return Info;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//inline bool TestPointVsSphere2D( const cPointVsSphere2D& Info ) {
+//	return Info.MagnitudeSquared < Info.RadiusSquared();
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//inline cPointVsSphere2D SolvePointVsSphere2D( const cPointVsSphere2D& _Info ) {
+//	cPointVsSphere2D Info = _Info;
+//	
+//	Info.Magnitude = Info.MagnitudeSquared.Sqrt();
+//	
+//	// TODO: More stuff needs to be done here, to correctly be considered a solve //
+//	
+//	//Info.InsideSphere = Info.MagnitudeSquared < Info.RadiusSquared;
+//	
+//	
+//	return Info;
+//}
+//// - ------------------------------------------------------------------------------------------ - //
+//inline cPointVsSphere2D SolvePointVsSphere2D( const Vector2D& Pos, const Vector2D& VsPos, const Real VsRadius ) {
+//	cPointVsSphere2D Info = StartPointVsSphere2D( Pos, VsPos, VsRadius );
+//	return SolvePointVsSphere2D( Info );
+//}
+//// - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
 // Finds the nearest point, returning the same point if inside //

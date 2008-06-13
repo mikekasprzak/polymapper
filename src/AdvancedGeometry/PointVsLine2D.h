bdcb23c1781fa1760009fa543b6f7d6441a017c0
mike <mike@b38dc06d-f929-334a-8341-2d1f71cecdda>
2008-06-13 01:00:37 +0000

    PointVsLine2D is the first class to get updated naming syntax, and an improved Nearest Point on a line implementation.
    git-svn-id: svn://10.0.50.55/polymapper/trunk@59 b38dc06d-f929-334a-8341-2d1f71cecdda

--- src/AdvancedGeometry/PointVsLine2D.h
+++ src/AdvancedGeometry/PointVsLine2D.h
@@ -6,30 +6,80 @@
 // - ------------------------------------------------------------------------------------------ - //
 
 // - ------------------------------------------------------------------------------------------ - //
-inline const Vector2D NearestPointOnLine2D( const Vector2D& p, const Vector2D& a, const Vector2D& b ) {
-	Vector2D ab = b - a;
-	Vector2D pa = a - p;
+//inline const Vector2D NearestPointOnLine2D( const Vector2D& p, const Vector2D& a, const Vector2D& b ) {
+//	Vector2D ab = b - a;
+//	Vector2D pa = a - p;
+//	
+//	Vector2D abNormal = ab;
+//	Real Width = abNormal.NormalizeRet();
+//	
+//	Real pdLength = pa * abNormal.Tangent();
+//	
+//	Vector2D d = p - (abNormal.Tangent() * pdLength);
+//	
+//	Vector2D ad = d - a;
+//	
+//	Real Offset = ad * abNormal;
+//	
+//	if ( Offset < Real::Zero ) {
+//		return a;
+//	}
+//	else if ( Offset > Width ) {
+//		return b;
+//	}
+//	else {
+//		return a + (abNormal * Offset);
+//	}
+//}
+// - ------------------------------------------------------------------------------------------ - //
+
+
+// - ------------------------------------------------------------------------------------------ - //
+struct cNearest_PointInfo_On_Line2D {
+	Vector2D Point;
 	
-	Vector2D abNormal = ab;
-	Real Width = abNormal.NormalizeRet();
+	Vector2D Line;
+	Vector2D LineNormal;
+	Real LineLength;
 	
-	Real pdLength = pa * abNormal.Tangent();
+	Vector2D CornerToPoint;
+	Real DistanceOnLine;
+};
+// - ------------------------------------------------------------------------------------------ - //
+inline const cNearest_PointInfo_On_Line2D Nearest_PointInfo_On_Line2D( const Vector2D& p, const Vector2D& a, const Vector2D& b ) {
+	cNearest_PointInfo_On_Line2D Info;
 	
-	Vector2D d = p - (abNormal.Tangent() * pdLength);
+	// Calculate our notable rays //
+	Info.Line = b - a; // ab //
+	Info.CornerToPoint = p - a; // ap //
+
+	// Normalize and Store the Line Normal and Length //
+	Info.LineNormal = Info.Line;
+	Info.LineLength = Info.LineNormal.NormalizeRet();
+
+	// Calculate how far 
+	Info.DistanceOnLine = Info.CornerToPoint * Info.LineNormal;
 	
-	Vector2D ad = d - a;
-	
-	Real Offset = ad * abNormal;
-	
-	if ( Offset < Real::Zero ) {
-		return a;
+	// Set Point to a position on the line //
+	if ( Info.DistanceOnLine < Real::Zero ) {
+		Info.Point = a;
 	}
-	else if ( Offset > Width ) {
-		return b;
+	else if ( Info.DistanceOnLine > Info.LineLength ) {
+		Info.Point = b;
 	}
 	else {
-		return a + (abNormal * Offset);
+		Info.Point = a + (Info.LineNormal * Info.DistanceOnLine);
 	}
+	
+	// Return the Info Structure //
+	return Info;
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
+inline const Vector2D NearestPointOnLine2D( const Vector2D& p, const Vector2D& a, const Vector2D& b ) {
+	cNearest_PointInfo_On_Line2D Info = Nearest_PointInfo_On_Line2D( p, a, b );
+	return Info.Point;
 }
 // - ------------------------------------------------------------------------------------------ - //
 

2ca32e2941f021a158b65fd263a356703f7e3e68
mike <mike@b38dc06d-f929-334a-8341-2d1f71cecdda>
2008-06-13 01:12:27 +0000

    Further refinements to the Edge Point Function.
    git-svn-id: svn://10.0.50.55/polymapper/trunk@61 b38dc06d-f929-334a-8341-2d1f71cecdda

--- src/AdvancedGeometry/PointVsChain2D.h
+++ src/AdvancedGeometry/PointVsChain2D.h
@@ -67,28 +67,32 @@ struct cNearestEdgePointInfoOnChain2D {
 // Get the nearest Inner Edge Point //
 inline const cNearestEdgePointInfoOnChain2D NearestEdgePointInfoOnChain2D( const Vector2D& Pos, const Vector2D* VsPoint, const size_t VsCount ) {
 	cNearestEdgePointInfoOnChain2D Info;
+	cNearest_PointInfo_On_Line2D LineInfo;
 	
 	if ( VsCount >= 1 ) {
-		Info.Point = Nearest_Point_On_Line2D( Pos, VsPoint[ VsCount - 1 ], VsPoint[ 0 ] );
-		Info.Normal = (VsPoint[ VsCount - 1 ] - VsPoint[ 0 ]);
+		LineInfo = Nearest_PointInfo_On_Line2D( Pos, VsPoint[ VsCount - 1 ], VsPoint[ 0 ] );
+		
+		Info.Point = LineInfo.Point;
+		Info.Normal = LineInfo.LineNormal;
+		
 		Info.EdgeDistanceSquared = (Info.Point - Pos).MagnitudeSquared();
 		Info.EdgeIndex = VsCount - 1;
 		
 		for ( size_t idx = 0; idx < VsCount - 1; idx++ ) {
-			Vector2D NewPoint = Nearest_Point_On_Line2D( Pos, VsPoint[ idx ], VsPoint[ idx + 1 ] );
-			Real NewDistanceSquared = (NewPoint - Pos).MagnitudeSquared();
+			LineInfo = Nearest_PointInfo_On_Line2D( Pos, VsPoint[ idx ], VsPoint[ idx + 1 ] );
+
+			Real NewDistanceSquared = (LineInfo.Point - Pos).MagnitudeSquared();
 			
 			if ( Info.EdgeDistanceSquared > NewDistanceSquared ) {
-				Info.Point = NewPoint;
-				Info.Normal = (VsPoint[ idx ] - VsPoint[ idx + 1 ]);
+				Info.Point = LineInfo.Point;
+				Info.Normal = LineInfo.LineNormal;
 				Info.EdgeDistanceSquared = NewDistanceSquared;
 				Info.EdgeIndex = idx;
 			}
 		}
 	}
 	
-	// Actually calculate the Normal //
-	Info.Normal = Info.Normal.Tangent().Normal();
+	Info.Normal = -Info.Normal.Tangent();
 	
 	return Info;
 }
